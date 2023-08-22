41118a1ec2faad5bbd16106e5d5ba9e1c6826be4
I putu Rama anadya <iputuramaanadya@IPs-Mac-mini.local>
2023-08-22 11:06:47 +0800

    change image, update readme

--- README.md
+++ README.md
@@ -68,3 +68,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# Deploy 
+
+## Deploy to github.io
+
+```
+npm run deploy --m "[commit description]"
+```
--- src/pages/home/component/header/HomeHeader.js
+++ src/pages/home/component/header/HomeHeader.js
@@ -1,11 +1,12 @@
 import './HomeHeader.scss';
 import ScrollAnimation from 'react-animate-on-scroll';
+import profileImage from './../../../../assets/images/profile.png';
 
 const HomeHeader = () => {
     return (
         <div className='home-header'>
             <div className='content'>
-                <img src='https://asset.kompas.com/crops/qEzp1lRjF_h3AOjVv8rhNwzoomU=/0x500:1000x1167/750x500/data/photo/2018/08/19/2853283469.jpg'/>
+                <img src={profileImage} alt='profile'/>
                 <div className='name'>I Putu Rama Anadya</div>
                 <div className='major'>Mobile and Front-end Developer</div>
             </div>
