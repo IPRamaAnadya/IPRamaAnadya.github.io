c73ab3a4f732e9d413d0430dff477cfb597cfe9f
I Putu Rama Anadya <71679235+IPRamaAnadya@users.noreply.github.com>
2024-07-18 03:13:35 +0000

    update cv

--- src/pages/home/component/skills/Skill.js
+++ src/pages/home/component/skills/Skill.js
@@ -18,19 +18,7 @@ const Skill = () => {
             <div className='skill'>
                 <img src='https://yt3.googleusercontent.com/ytc/AGIKgqM8zh66fZqGKeTkopHaU9GM4zvyuFnQhXThr37u=s900-c-k-c0x00ffffff-no-rj'/>
                 <div>Flutter</div>
-            {/* </div>
-            <div className='skill'>
-                <img src='https://react.dev/images/og-home.png'/>
-                <div>React</div>
             </div>
-            <div className='skill'>
-                <img src='https://e7.pngegg.com/pngimages/666/502/png-clipart-brand-php-logo-computer-file-product-breeze-blue-text.png'/>
-                <div>PHP</div>
-            </div>
-            <div className='skill'>
-                <img src='https://images.ctfassets.net/mrop88jh71hl/55rrbZfwMaURHZKAUc5oOW/9e5fe805eb03135b82e962e92169ce6d/python-programming-language.png?w=750&h=750&q=100&fm=png'/>
-                <div>Python</div>
-            </div> */}
         </div>
         <div className='other-skills'>Other skills</div>
         <div className='skills-wrapper'>
