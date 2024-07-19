45e9d7ef35f13cf614c9b6257b2080f7b18d5752
iputuramaanadya <iputuramaanadya@gmail.com>
2024-07-19 01:50:51 +0000

    change active value

--- src/pages/home/component/qualification/Qualification.js
+++ src/pages/home/component/qualification/Qualification.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const Qualification = () => {
 
-    const [qualificationSelected, setQualificationSelected] = useState('education');
+    const [qualificationSelected, setQualificationSelected] = useState('works');
 
     const educations = [
         {
